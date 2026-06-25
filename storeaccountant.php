d2ac083e4cd60fb9106ef3929b3a0fb616dafa9d
Thomas Baier <thomas.baier@launch-lab.de>
2026-06-25 17:41:55 +0200

    chore: release prep

--- storeaccountant.php
+++ storeaccountant.php
@@ -3,7 +3,7 @@
  * Plugin Name: StoreAccountant
  * Plugin URI: https://storeaccountant.launch-lab.de/
  * Description: Accounting workflow plugin for WooCommerce.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: LaunchLab GmbH
  * Author URI: https://launch-lab.de
  * Text Domain: storeaccountant
@@ -47,7 +47,7 @@ final readonly class StoreAccountant {
 	/**
 	 * Current plugin version.
 	 */
-	public const PLUGIN_VERSION = '1.0.0';
+	public const PLUGIN_VERSION = '1.0.1';
 
 	/**
 	 * Minimum supported PHP version.
